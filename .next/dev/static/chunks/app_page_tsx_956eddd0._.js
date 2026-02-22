fe679c86bb204e0593323a2bd463df688fbcedd5
livinusonyenso <livinusonyenso@gmail.com>
2026-02-22 20:28:50 +0100

    removed cached .next

--- .next/dev/static/chunks/app_page_tsx_956eddd0._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/components_9a25649e._.js",
-  "static/chunks/node_modules_next_dist_7eb093c7._.js"
-],
-    source: "dynamic"
-});
--- .next/server/app/_global-error.segments/_tree.segment.rsc
+++
@@ -1,1 +0,0 @@
-0:{"buildId":"Rolx4aIIToO4jOR4-Gydc","tree":{"name":"","paramType":null,"paramKey":"","hasRuntimePrefetch":false,"slots":{"children":{"name":"__PAGE__","paramType":null,"paramKey":"__PAGE__","hasRuntimePrefetch":false,"slots":null,"isRootLayout":false}},"isRootLayout":false},"staleTime":300}
