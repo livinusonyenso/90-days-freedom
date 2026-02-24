d2518820bd4bff002f16702c6a6761038c05d96f
livinusonyenso <livinusonyenso@gmail.com>
2026-02-24 20:09:01 +0100

    wip footer

--- .next/dev/server/app/page.js
+++ .next/dev/server/app/page.js
@@ -1,6 +1,6 @@
 var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/app/page.js")
 R.c("server/chunks/ssr/node_modules_f83c0a42._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__1492e375._.js")
+R.c("server/chunks/ssr/[root-of-the-server]__cb710601._.js")
 R.c("server/chunks/ssr/node_modules_next_dist_7381059c._.js")
 R.c("server/chunks/ssr/[externals]__7f148858._.js")
 R.c("server/chunks/ssr/node_modules_next_dist_client_components_builtin_global-error_ece394eb.js")
