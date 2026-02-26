c710c36f4c00f7aa493dcef838d5db5bf8ed8a8e
livinusonyenso <livinusonyenso@gmail.com>
2026-02-26 16:13:10 +0100

    fix hire talent UI

--- .next/dev/types/validator.ts
+++ .next/dev/types/validator.ts
@@ -54,6 +54,15 @@ type LayoutConfig<Route extends LayoutRoutes = LayoutRoutes> = {
   type __Unused = __Check
 }
 
+// Validate ../../../app/hire-talent/page.tsx
+{
+  type __IsExpected<Specific extends AppPageConfig<"/hire-talent">> = Specific
+  const handler = {} as typeof import("../../../app/hire-talent/page.js")
+  type __Check = __IsExpected<typeof handler>
+  // @ts-ignore
+  type __Unused = __Check
+}
+
 // Validate ../../../app/login/page.tsx
 {
   type __IsExpected<Specific extends AppPageConfig<"/login">> = Specific
